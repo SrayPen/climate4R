3084757683e11556ede5a9246e0b37794fac3d1e
zequihg50 <zequi3000@hotmail.com>
2020-03-18 09:58:05 +0100

    Seems to work

--- conda/build.sh
+++ conda/build.sh
@@ -1,18 +1,19 @@
 # for convertR
 export UDUNITS2_LIB=${PREFIX}/lib
+export UDUNITS2_INCLUDE=${PREFIX}/include
 export CPATH=${PREFIX}/include
 
 # for rjava
-# export LD_LIBRARY_PATH=${PREFIX}/lib:${PREFIX}/lib/server:$PREFIX/lib/R/library/rJava/libs:$PREFIX/jre/lib/amd64/server
+#export LD_LIBRARY_PATH=${PREFIX}/lib:${PREFIX}/lib/server:$PREFIX/lib/R/library/rJava/libs:$PREFIX/jre/lib/amd64/server
 
-R --vanilla -e 'install.packages("rJava", repos="https://cloud.r-project.org/")'
 R --vanilla -e 'install.packages("devtools", repos="https://cloud.r-project.org/")'
-R --vanilla -e 'install.packages("udunits2", repos="https://cloud.r-project.org/")'
-R --vanilla -e 'install.packages("ncdf4", repos="https://cloud.r-project.org/")'
-R --vanilla -e 'install.packages("gh", repos="https://cloud.r-project.org/")'
-R --vanilla -e 'install.packages("matrix", repos="https://cloud.r-project.org/")'
-R --vanilla -e 'install.packages("geojsonio", repos="https://cloud.r-project.org/")'
-R --vanilla -e 'install.packages("rgdal", repos="https://cloud.r-project.org/")'
+#R --vanilla -e 'install.packages("rJava", repos="https://cloud.r-project.org/")'
+#R --vanilla -e 'install.packages("udunits2", repos="https://cloud.r-project.org/")'
+#R --vanilla -e 'install.packages("ncdf4", repos="https://cloud.r-project.org/")'
+#R --vanilla -e 'install.packages("gh", repos="https://cloud.r-project.org/")'
+#R --vanilla -e 'install.packages("matrix", repos="https://cloud.r-project.org/")'
+#R --vanilla -e 'install.packages("geojsonio", repos="https://cloud.r-project.org/")'
+#R --vanilla -e 'install.packages("rgdal", repos="https://cloud.r-project.org/")'
 R --vanilla -e 'library(devtools);install_github("SantanderMetGroup/climate4R.UDG@v0.1.0")'
 R --vanilla -e 'library(devtools);install_github("SantanderMetGroup/loadeR.java")'
 R --vanilla -e 'library(devtools);install_github("SantanderMetGroup/loadeR@devel")' 
